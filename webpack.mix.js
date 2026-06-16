e05aac7c0c2a41328696432453f36b845e6d2ba9
Rabii43 <Rabii12terres@gmail.com>
2026-06-16 21:44:48 +0100

    Install authentication with Laravel Breeze

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .babelConfig({
+        presets: [
+            ['@babel/preset-env', { targets: { node: 'current' } }]
+        ],
+        plugins: ['@babel/plugin-proposal-optional-chaining']
+    })
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('tailwindcss'),
+        require('autoprefixer'),
     ]);
